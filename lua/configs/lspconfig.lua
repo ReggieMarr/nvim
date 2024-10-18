764468d2b1fdab702a8c353bcdd0e5208fde968f
Reg Marr <reginald.marr@reactiondynamics.space>
2024-10-18 16:04:37 -0400

    Add vimacs and apply migrate.sh for v2.5 support

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,24 +1,15 @@
--- load defaults i.e lua_lsp
-require("nvchad.configs.lspconfig").defaults()
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
-local servers = { "html", "cssls" }
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- lsps with default config
+-- if you just want default config for the servers then put them in a table
+local servers = { "cssls", "clangd", "rust_analyzer", "cmake" }
+--                                    ^^^^^^^^^^^^^^^^-- TODO: Set up with rust-tools.nvim
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
+    on_attach = on_attach,
+    capabilities = capabilities,
   }
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
